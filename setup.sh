1a85f9b079800dbdbaeb3b7edd699407b8f77da7
OwnageTechGeek <OwnageTechGeek@users.noreply.github.com>
2017-01-08 11:49:20 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,3 +3,6 @@
 mv ~/sources.list /etc/apt/sources.list
 apt-get update -y
 apt-get install openssh -y
+adduser bhogue #Requires manual entry.
+passwd bhogue #Requires manual entry. (Don't want to hardcode passwords.)
+
